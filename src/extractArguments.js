1c9c5a2163bab576f37be38f4fda397ac0a2e014
merceyz <merceyz@users.noreply.github.com>
2019-05-04 22:07:46 +0200

    refactor: split argument extraction to its own file

---
+++ src/extractArguments.js
@@ -0,0 +1,15 @@
+module.exports = (t, args) => {
+  const newArguments = [];
+
+  for (const argument of args) {
+    if (argument.type === 'ObjectExpression') {
+      for (const p of argument.properties) {
+        newArguments.push(t.LogicalExpression('&&', p.value, p.key));
+      }
+    } else {
+      newArguments.push(argument);
+    }
+  }
+
+  return newArguments;
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
+const extractArguments = require('./extractArguments');
+
 module.exports = ({ types: t }) => {
   return {
     visitor: {
       CallExpression: ({ node }) => {
         if (node.callee.type === 'Identifier' && node.callee.name === 'clsx') {
-          const newArguments = [];
-
-          for (const argument of node.arguments) {
-            if (argument.type === 'ObjectExpression') {
-              for (const p of argument.properties) {
-                newArguments.push(t.LogicalExpression('&&', p.value, p.key));
-              }
-            } else {
-              newArguments.push(argument);
-            }
-          }
-
-          node.arguments = newArguments;
+          node.arguments = extractArguments(t, node.arguments);
         }
       },
     },
