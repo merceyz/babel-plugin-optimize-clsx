3d635964d4e00526d6015a3c02cd44d9c444cd53
merceyz <merceyz@users.noreply.github.com>
2019-06-08 23:52:35 +0200

    feat(combinestrings): support template literals

--- src/visitors/combineStringLiterals.js
+++ src/visitors/combineStringLiterals.js
@@ -6,13 +6,74 @@ function combineStringsInArray(array) {
     return array;
   }
 
-  const [match, noMatch] = _.partition(array, t.isStringLiteral);
+  const [match, noMatch] = _.partition(
+    array,
+    item => t.isStringLiteral(item) || t.isTemplateLiteral(item),
+  );
 
   if (match.length < 2) {
     return array;
   }
 
-  return [match.reduce((prev, curr) => t.stringLiteral(prev.value + ' ' + curr.value)), ...noMatch];
+  const [strings, templates] = _.partition(match, t.isStringLiteral);
+
+  const expressions = [];
+  const quasis = [];
+
+  // Combine string literals
+  if (strings.length > 0) {
+    quasis.push(
+      templateElement(
+        strings.reduce((prev, curr) => t.stringLiteral(prev.value + ' ' + curr.value)).value,
+        true,
+      ),
+    );
+  }
+
+  // Combine template literals
+  templates.forEach(item => {
+    if (item.expressions.length === 0) {
+      const newValue = item.quasis.reduce((prev, curr) =>
+        templateElement(prev.value.raw + ' ' + curr.value.raw, true),
+      );
+
+      if (quasis.length === 0) {
+        quasis.push(newValue);
+        return;
+      }
+
+      const prevItem = quasis[quasis.length - 1];
+      quasis[quasis.length - 1] = templateElement(
+        prevItem.value.raw + ' ' + newValue.value.raw,
+        true,
+      );
+      return;
+    }
+
+    item.quasis.forEach(item => {
+      if (quasis.length !== 0) {
+        const prevItem = quasis[quasis.length - 1];
+
+        if (item.tail === false && prevItem.tail) {
+          quasis[quasis.length - 1] = templateElement(
+            prevItem.value.raw + ' ' + item.value.raw,
+            true,
+          );
+          return;
+        }
+      }
+
+      quasis.push(item);
+    });
+
+    expressions.push(...item.expressions);
+  });
+
+  if (expressions.length === 0 && quasis.length === 1) {
+    return [t.stringLiteral(quasis[0].value.raw), ...noMatch];
+  }
+
+  return [t.templateLiteral(quasis, expressions), ...noMatch];
 }
 
 const arrayVisitor = {
@@ -41,3 +102,13 @@ const visitor = {
 export default (path, options) => {
   path.traverse(visitor, { options });
 };
+
+function templateElement(value, tail = false) {
+  return {
+    type: 'TemplateElement',
+    value: {
+      raw: value,
+    },
+    tail,
+  };
+}
